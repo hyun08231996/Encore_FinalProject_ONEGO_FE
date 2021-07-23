c6fc3f34830552cf99fc2b8b2f33350af0688f22
yeram214 <njuko@naver.com>
2021-07-23 17:24:30 +0900

    OneGo

---
+++ src/components/buttons/SearchBtn.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-btn icon @click="showSearch">
+		<v-icon>mdi-magnify</v-icon>
+	</v-btn>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+
+	export default Vue.extend({
+		methods:{
+			showSearch(){
+			console.log("hello")
+			}
+		}
+	})
+</script>
+
+<style scoped>
+
+</style>
