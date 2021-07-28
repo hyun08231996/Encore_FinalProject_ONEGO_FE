105c54973ef4e410d0631f9f3944c5e84338704c
yeram214 <njuko@naver.com>
2021-07-28 17:12:27 +0900

    글 내용

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-card>
+	<v-card class="header-padding">
 		<!-- navbar -->
 		<v-app-bar
 		fixed
@@ -9,13 +9,15 @@
 		>
 		<v-app-bar-nav-icon @click="drawer = true; openMenu()"></v-app-bar-nav-icon>
 
-		<a href="/"><img src="@/assets/logo/onego_logo.jpeg" width="20%" height="20%"></a>
+		<a href="/"><img src="@/assets/logo/onego_logo.jpeg"
+			width="120px" height="43.2px"
+			style="vertical-align:middle;"></a>
 
 		<v-spacer></v-spacer>
 		<div v-if="isAuth"><logout-btn @logout="isAuth = $event"/></div>
 		<div v-else><signup-btn /><login-btn @login="isAuth = $event" /></div>
 
-		<search-btn />
+		<search-modal @openDrawer="drawer = $event" />
 		</v-app-bar>
 
 		<!-- sidebar -->
@@ -23,25 +25,27 @@
 			v-model="drawer"
 			temporary
 			fixed
-			fluid style="height: 100vh;"
+			fluid style="height: 100vh; width: 250px;"
 			app
 			>
 			<br><br>
 			<div v-if="isAuth">
-				<div class="side-itms" style="padding-left:5%;">
-					<v-list-item-avatar size=90>
+				<div class="side-itms">
+					<a href="/myprofile">
+					<v-list-item-avatar size=90 class="mx-auto">
 						<img src="https://randomuser.me/api/portraits/women/82.jpg">
 					</v-list-item-avatar>
+					</a>
 				</div>
 				<div class="side-itms">
 					<v-list-item-content>
-						<v-list-item-title>Mary Jane</v-list-item-title>
+						<v-list-item-title>{{user.nickname}}</v-list-item-title>
 					</v-list-item-content>
 				</div>
 			</div>
 			<div v-else>
-				<div class="side-itms" style="padding-left:5%;">
-					<v-list-item-avatar size=90>
+				<div class="side-itms">
+					<v-list-item-avatar size=90 class="mx-auto">
 						<img src="@/assets/logo/onego_mac.png">
 					</v-list-item-avatar>
 				</div>
@@ -55,18 +59,17 @@
 			<br>
 			<div class="side-itms">
 				<div v-if="isAuth">
-					<v-btn class="ma-2" rounded outlined color="#00d5aa">
+					<v-btn id="write-btn" rounded outlined color="#00d5aa">
 						글쓰기
 					</v-btn>
 				</div>
-
 			<br>
 			<v-divider></v-divider>
 			<br>
-				<v-btn id="menu-btn" retain-focus-on-click rounded outlined color="grey" @click="openMenu" >
+				<v-btn id="menu-btn" rounded outlined color="#00d5aa" @click="openMenu" >
 					메뉴
 				</v-btn>&nbsp;&nbsp;
-				<v-btn retain-focus-on-click rounded outlined color="grey" @click="openCategory">
+				<v-btn id="cate-btn" rounded outlined @click="openCategory">
 					카테고리
 				</v-btn>
 			</div>
@@ -83,33 +86,37 @@
 						</v-list-item-content>
 					</v-list-item>
 				</div>
-				<br><br>
+				<br>
 				<div v-if="isAuth" class="side-itms">
 					<v-list-item
 					v-for="item in menusAL"
 					:key="item.title">
 						<v-list-item-content>
-							<v-btn text><v-list-item-title>{{ item.title }}</v-list-item-title></v-btn>
+							<v-btn text :href="item.link"><v-list-item-title>{{ item.title }}</v-list-item-title></v-btn>
 						</v-list-item-content>
 					</v-list-item>
 				</div>
 			</v-list>
 
-			<v-list dense v-if="showCate">
+			<v-list dense v-if="showCate" class="side-itms">
 					<div>
 						<v-list-item
 						v-for="item in categories"
 						:key="item.title">
 							<v-list-item-content>
-								<v-btn text><v-list-item-title>{{ item.title }}</v-list-item-title></v-btn>
+								<v-btn text :href="item.link"><v-list-item-title>{{ item.title }}</v-list-item-title></v-btn>
 							</v-list-item-content>
 						</v-list-item>
 					</div>
 			</v-list>
 
-			<div class="side-itms">
-				<div v-if="isAuth" id="side-footer1"><logout-btn @logout="isAuth = $event"/></div>
-				<div v-else id="side-footer2"><signup-btn /><login-btn @login="isAuth = $event" /></div>
+			<div id="side-footer">
+				<div v-if="isAuth" class="side-footer-btns">
+					<div><setting-btn /><logout-btn @logout="isAuth = $event"/></div>
+				</div>
+				<div v-else class="side-footer-btns">
+					<div><signup-btn id="signup-btn" /><login-btn id="login-btn" @login="isAuth = $event" /></div>
+				</div>
 			</div>
 
 		</v-navigation-drawer>
@@ -122,7 +129,8 @@
 	import LoginBtn from '@/components/buttons/LoginBtn.vue'
 	import LogoutBtn from '@/components/buttons/LogoutBtn.vue'
 	import SignupBtn from '@/components/buttons/SignupBtn.vue'
-	import SearchBtn from '@/components/buttons/SearchBtn.vue'
+	import SettingBtn from '@/components/buttons/SettingBtn.vue'
+	import SearchModal from '@/views/SearchModal.vue'
 
 	export default Vue.extend({
 		data: () => ({
@@ -136,38 +144,63 @@
 				{ title: '최신글', link: '/article'},
 			],
 			menusAL: [
-				{ title: '내 프로필'},
-				{ title: '내 원고'},
-				{ title: '스크랩'},
+				{ title: '내 프로필', link: '/myprofile'},
+				{ title: '내 원고', link: '/myonego'},
+				{ title: '스크랩', link: '/scrap'},
 			],
 			categories:[
-				{title:'일상'},
-				{title:'시사·이슈'},
+				{title:'일상', link: '/myContent'},
+				{title:'시사·이슈', link: '/content'},
 				{title:'IT 트렌드'},
 				{title:'요리·레시피'},
 				{title:'운동·건강'},
 				{title:'사랑·이별'}
-			]
+			],
+			user:{
+				name:'Mary Jane',
+				nickname:'Mary',
+				email:'mj123@gmail.com',
+				intro:'Hello, I am an avid writer',
+				pic:"https://randomuser.me/api/portraits/women/82.jpg"
+			},
 		}),
 		name: "Header",
 		components:{
-			'login-btn':LoginBtn, 'logout-btn':LogoutBtn, 'signup-btn':SignupBtn,
-			'search-btn':SearchBtn
+			'login-btn':LoginBtn, 'logout-btn':LogoutBtn, 'signup-btn':SignupBtn, 'setting-btn':SettingBtn,
+			'search-modal':SearchModal
 		},
 		methods:{
 			openCategory(){
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				document.getElementById('menu-btn')!.style.color = "#9E9E9E";
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				document.getElementById('cate-btn')!.style.color = "#00d5aa";
 				this.showMenu = false;
 				this.showCate = true;
 			},
 			openMenu(){
-				this.showCate = false;
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				document.getElementById('menu-btn')!.style.color = "#00d5aa";
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				document.getElementById('cate-btn')!.style.color = "#9E9E9E";
 				this.showMenu = true;
+				this.showCate = false;
+
 			}
 		}
 	})
 </script>
 
 <style>
+	.row {
+		display: flex;
+		flex-wrap: wrap;
+		flex: 0 !important;
+		margin: 0 !important;
+	}
+	.header-padding .v-toolbar__content{
+		padding-right:5px;
+	}
 	.side-itms{
 		text-align:center;
 
@@ -175,23 +208,33 @@
 	.v-btn::before{
 		background-color: transparent !important;
 	}
-	.v-btn:hover,
-	.v-btn:focus,
-	.v-btn:active{
+	.v-btn:hover{
 		color: #00d5aa !important;
 	}
-	#side-footer2{
-		position:fixed !important;
-		bottom:3%;
-		left:10%;
+	#write-btn:hover{
+		color:#02bf99 !important;
 	}
-	#side-footer1{
-		position:fixed !important;
-		bottom:3%;
-		left:28%;
+	#side-footer{
+		width:250px;
+	}
+	.side-footer-btns{
+		position: absolute !important;
+		margin-left: auto;
+		margin-right: auto;
+		left: 0;
+		right: 0;
+		bottom: 3%;
+		text-align: center;
 	}
 	#menu-btn{
 		padding-left:30px !important;
 		padding-right:30px !important;
+	}
+	#login-btn{
+		padding-left:23px !important;
+		padding-right:23px !important;
+	}
+	.v-navigation-drawer {
+		z-index: 999999 !important;
 	}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Main from '@/views/Main.vue'
+import SearchResult from '@/views/SearchResult.vue'
 import Scrap from '@/views/Scrap.vue'
 import Article from '@/views/Article.vue'
-	
+import Setting from '@/views/Setting.vue'
+import ChangePassword from '@/views/ChangePassword.vue'
+import EditProfile from '@/views/EditProfile.vue'
+import MyOnego from '@/views/MyOnego.vue'
+import MyProfile from '@/views/MyProfile.vue'
+import UserProfile from '@/views/UserProfile.vue'
+import MyContent from '@/views/MyContent.vue'
+import Content from '@/views/Content.vue'
+
 Vue.use(VueRouter)
-	
+
 const routes: Array<RouteConfig> = [
 	{
 		path: "/",
+		name: "Main",
 		component: Main
+	},
+	{
+		path: "/search/:search",
+		name: "SearchResult",
+		component: SearchResult,
+		props: true
 	},
 	{
 		path: "/scrap",
@@ -18,6 +34,52 @@ const routes: Array<RouteConfig> = [
 	{
 		path: "/article",
 		component: Article
+	},
+	{
+		path: "/setting",
+		name: "Setting",
+		component: Setting
+	},
+	{
+		path:"/change-pass",
+		name: "ChangePassword",
+		component: ChangePassword
+	},
+	{
+		path:"/edit-prof",
+		name: "EditProfile",
+		component: EditProfile
+	},
+	{
+		path:"/myonego",
+		name: "MyOnego",
+		component: MyOnego
+	},
+	{
+		path:"/myprofile",
+		name: "MyProfile",
+		component: MyProfile,
+		meta: {
+			headerClass: 'myprof-header-color'
+		}
+	},
+	{
+		path:"/userprofile",
+		name: "UserProfile",
+		component: UserProfile,
+		meta:{
+			headerClass: 'myprof-header-color'
+		}
+	},
+	{
+		path:"/myContent",
+		name: "MyContent",
+		component: MyContent
+	},
+	{
+		path:"/content",
+		name: "Content",
+		component: Content
 	}
 ]
 
