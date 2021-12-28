d25ea38a1b3ac0109a8ff8e47041677e374a8482
Seung Hyun (Peter) Nam <hyun08231996@gmail.com>
2021-12-29 02:27:12 +0900

    Update DarkModeSwitch.vue

--- src/components/buttons/write/DarkModeSwitch.vue
+++ src/components/buttons/write/DarkModeSwitch.vue
@@ -1,12 +1,22 @@
 <template>
-
-	  <v-switch class="mt-5 ml-4"
-		v-model="switch1"
-		color="#00d5aa"
-		dense
-		inset
-	  />
-
+	<v-tooltip bottom>
+		<template v-slot:activator="{ on, attrs }">
+			<div v-bind="attrs" v-on="on" style="margin-left:13px;">
+				<!-- <v-switch class="mt-5 ml-4"
+					v-model="switch1"
+					color="#00d5aa"
+					dense
+					inset
+				/> -->
+				<toggle-button @change="switchDark" :color="{checked: '#696969', unchecked: '#E0E0E0'}" 
+					:labels="{checked: '🌙', unchecked: '☀️'}" 
+					:switch-color="{checked: '#121212', unchecked: '#FFFFFF'}"
+					font-size="11"/>
+			</div>
+		</template>
+		<span>{{tooltip}}</span>
+	</v-tooltip>
+	
 
 </template>
 
@@ -16,24 +26,34 @@
 
 	@Component
 	export default class DarkModeSwitch extends Vue {
-		switch1 = false
+		switch = false
+		tooltip = "Activate DarkMode"
 
-		@Watch('switch1')
-		watchSwitch1():void{
+		@Watch('switch')
+		watchSwitch():void{
 			//console.log(this.switch1)
-			if(this.switch1 == true){
+			if(this.switch == true){
 				setTimeout(()=>{
 					this.$vuetify.theme.dark = true
 				},10)
 				
 				eventBus.$emit('toDark',true)
+
+				this.tooltip = "Deactivate DarkMode"
 			}else{
 				setTimeout(()=>{
 					this.$vuetify.theme.dark = false
 				},10)
 				
 				eventBus.$emit('toLight',false)
+
+				this.tooltip = "Activate DarkMode"
 			}
+		}
+
+		switchDark():void{
+			//console.log("hello")
+			this.switch = !this.switch
 		}
 
 	}
